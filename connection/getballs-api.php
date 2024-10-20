638b22840363afa0e783bd43a96524415508f996
Zaifal <151502524+Zaifal@users.noreply.github.com>
2024-10-20 19:20:09 +0700

    upload api get untuk balls dan link stream

---
+++ connection/getballs-api.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json'); // Mengatur header untuk JSON
+
+// Sambungkan ke database
+$conn = new mysqli('localhost', 'aurora', '@ur0r45TMK6j4y4', 'aurora');
+// Cek koneksi
+if ($conn->connect_error) {
+    echo json_encode(array("error" => "Connection failed: " . $conn->connect_error));
+    exit();
+}
+
+// Menggunakan prepared statement
+$stmt = $conn->prepare("SELECT balls FROM ball_counter ORDER BY id DESC LIMIT 1");
+$stmt->execute();
+$result = $stmt->get_result();
+
+
+$stmt->close();
+$conn->close();
+?>
